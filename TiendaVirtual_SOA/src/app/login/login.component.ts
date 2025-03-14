047112bea676ea8db179311c1b5e8d2e9f635637
CamiiUribeee <acuribel@ufpso.edu.co>
2025-03-13 22:43:32 -0500

    CAMBIOS: GTS-6 Implementacion de validaciones en los formularios del login y el registro

--- TiendaVirtual_SOA/src/app/login/login.component.ts
+++ TiendaVirtual_SOA/src/app/login/login.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],  
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
 
+  enviarFormulario() {
+    if (this.loginForm.valid) {
+      console.log('Formulario enviado:', this.loginForm.value);
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
+  }
+  
 }
+
