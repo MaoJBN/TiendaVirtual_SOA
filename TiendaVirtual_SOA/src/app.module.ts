b4f7accd3f871365efb650c686be3028aeb87b9a
MaoB08 <maonotes08@gmail.com>
2025-03-14 20:11:16 -0500

    Revert "GTS-18 Establecer la conexion de FireBase con el proyecto"
    This reverts commit f008142d4e72f0534f57ee92f8d64477137f451b.

--- TiendaVirtual_SOA/src/app.module.ts
+++ TiendaVirtual_SOA/src/app.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { routes } from './app/app.routes'; 
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
-import { firebaseConfig } from './app/firebase.config';
 @NgModule({
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes)
   ],
-  providers: [provideFirebaseApp(() => initializeApp(firebaseConfig))],
+  providers: [],
   bootstrap: [] 
 })
 export class AppModule { }
