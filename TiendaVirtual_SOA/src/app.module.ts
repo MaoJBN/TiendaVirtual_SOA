7a675e2463c4ea3d2868df07d9610880a4a1db00
KevinSpinell <kevinnallicarrascal@gmail.com>
2025-03-15 10:22:54 -0500

    GTS-7-Crear-servicio-de-autenticación-en-Angular-con-Firebase-Authentication

--- TiendaVirtual_SOA/src/app.module.ts
+++ TiendaVirtual_SOA/src/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { routes } from './app/app.routes'; 
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
-import { firebaseConfig } from './app/firebase.config';
+import { firebaseConfig } from './environments/environment';
 @NgModule({
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
   ],
-  providers: [provideFirebaseApp(() => initializeApp(firebaseConfig))],
+  providers: [provideFirebaseApp(() => initializeApp(firebaseConfig.firebaseConfig))],
   bootstrap: [] 
 })
 export class AppModule { }
